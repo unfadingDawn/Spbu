070ec4616b2196c03f0706c82523ad377a79877e
Yuriy Litvinov <bmwproexpert@gmail.com>
2025-06-02 03:40:03 +0300

    Deleted task2 from parallels

--- parralells/task2/src/program.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <mutex>
-
-namespace program {
-    void make_sum(std::vector<int>& nums, const std::vector<int>& indexes);
-}
--- parralells/task2/src/util.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-
-namespace util {
-    std::vector<int> rand_indexes(int n);
-}
